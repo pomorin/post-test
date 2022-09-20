46bca82344e8c50bdc043e79b1bf422db0dd6934
Pierre-Olivier Morin <pmorin@gmail.com>
2022-09-20 18:56:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+async function my_post(event) {
+
+    event.preventDefault();
+
+    const restletid = document.getElementById('input-restletid').value;
+    const token = document.getElementById('input-tokenid').value;
+    const url = `https://driver01.azurewebsites.net?restletid=${restletid}&token=${token}`;
+
+    let inputFile = document.querySelector('input[type="file"]');
+
+    let formData = new FormData();
+    formData.append('firstname', 'John');
+    formData.append('lastname', 'Doe');
+    formData.append('file', inputFile.files[0])
+
+    fetch(url, {
+        method: "POST",
+        body: formData,
+    })
+    .then((response) => {
+        console.log('response', response)   
+    })
+    .catch((error) => {
+        console.log('error', error);
+    });
+}
+
+var form = document.getElementById("form-test");
+form.addEventListener("submit", my_post, true);
